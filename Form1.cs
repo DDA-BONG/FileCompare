5b61a70ac2ecd0cfa5373e84094367d5c1d37d5c
형규 윤 <yhk8835@naver.com>
2026-04-16 10:10:42 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace FileCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
